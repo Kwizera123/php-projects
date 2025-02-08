42c8cfdb47ae1ebe264942860ad87aa42b879e8d
Kwizera Theo <kwizerasinapy49@gmail.com>
2025-02-08 17:14:12 +0200

    php Registration,login,signup and logout as well as home page added

--- signup-login-logout/login.php
+++ signup-login-logout/login.php
@@ -1,43 +1,28 @@
 <?php
-
-$success=0;
-$user=0;
+$login=0;
+$invalid=0;
 
   if($_SERVER['REQUEST_METHOD']=='POST'){
     include 'connect.php';
     $username=$_POST['username'];
     $password=$_POST['password'];
 
-    // $sql = "insert into `registrationform`(username,password) values('$username','$password')";
-    // $result=mysqli_query($mysqli,$sql);
-
-    // if($result){
-    //   echo "Data inserted successfully";
-    // }else{
-    //   //die(mysqli_error($mysqli));
-    //   echo $mysqli->connect_error;
-    // }
-
-    $sql = "select * from `registrationform` where username = '$username'";
+    $sql = "select * from `registrationform` where username = '$username' and password = '$password'";
 
     $result = mysqli_query($mysqli,$sql);
       if($result){
         $num=mysqli_num_rows($result);
         if($num>0){
-          //echo "User already exists";
-          $user=1;
+          $login=1;
+
+          session_start();
+          $_SESSION['username']=$username;
+          header('location:home.php');
+          
         }else{
-         $sql = "insert into `registrationform`(username,password) values('$username','$password')";
-         $result=mysqli_query($mysqli,$sql);
-         if($result){
-             //echo "Signup successful";
-             $success=1;
-             header("Location:login.php");
-           }else{
-         die(mysqli_error($mysqli));
-         
-           }
+        $invalid=1;
         }
+        
       }
 
   }
@@ -49,15 +34,16 @@ $user=0;
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sign Up</title>
+    <title>Login Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <?php 
-      if($user){
+
+  <?php 
+      if($login){
         echo '
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-  <strong>Ohoo Sorry!</strong> User already exist!.
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+  <strong>Great!</strong> You have successfully Logged In.
   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
         ';
@@ -65,18 +51,19 @@ $user=0;
     ?>
 
 <?php 
-      if($success){
+      if($invalid){
         echo '
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-  <strong>Great!</strong> You have successfully signed up.
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+  <strong>Ohoo Sorry!</strong> Invalid username or passwor.
   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
         ';
       }
     ?>
+
     <div class="container mt-5">
-      <h2 class="lead fw-lighter text-center">Sign-Up Form</h2>
-    <form action="sign.php" method="post">
+      <h2 class="lead fw-lighter text-center">Login to our Website</h2>
+    <form action="login.php" method="post">
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label fw-lighter">Username</label>
     <input type="text" name="username" class="form-control fw-lighter" placeholder="Enter your Username" aria-describedby="emailHelp">
@@ -87,7 +74,7 @@ $user=0;
     <input type="password" name="password" class="form-control fw-lighter" placeholder="Enter your password">
   </div>
 
-  <button type="submit" class="btn btn-primary">Sign Up</button>
+  <button type="submit" class="btn btn-primary">Login</button>
 </form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
